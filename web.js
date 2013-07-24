db2df1dbab2e9b49fb10c92d53ba81d1d11a8b41
Ubuntu <ubuntu@ip-172-31-17-28.us-west-2.compute.internal>
2013-07-24 04:08:00 +0000

    updated files

--- web.js
+++ web.js
@@ -3,12 +3,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use('/img', express.static(__dirname + '/img'));
+
 app.get('/', function(request, response) {
   var outString = fs.readFileSync("index.html").toString();
   response.send(outString);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
